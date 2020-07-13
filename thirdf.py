ed98f88dd20339608444bbb58e97a9c2f25525dc
liumengling <ling2577879400@163.com>
2020-07-13 20:17:34 +0800

    第三次作业文件改

--- thirdf.py
+++ thirdf.py
@@ -6,7 +6,7 @@ class PersonInfo(object):
         self.__gender = gender
         self.__age = age
         self.__number = number
-        self.count += 1
+        PersonInfo.count += 1
 
     def __str__(self):
         return "%s\t  %s\t  %s\t %s" % (self.__name, self.__gender,self.__age,self.__number)
@@ -17,12 +17,19 @@ class PersonInfo(object):
 
 def open_file():
     out_list = []
-    out_file = open("test.txt", "r",encoding='utf-8')
-    for line in out_file:
-        person_list = line.split()
-        person = PersonInfo(person_list[0],person_list[1],person_list[2],person_list[3])
-        out_list.append(person)
+    #out_file = open("test.txt", "r",encoding='utf-8')
+    with open("test.txt", "r", encoding="utf-8") as out_file:
+        for line in out_file:
+            person_list = line.split()
+            person = PersonInfo(person_list[0], person_list[1], person_list[2], person_list[3])
+            out_list.append(person)
     return out_list
+
+
+def get_input():
+    out_rule = int(input("请输入相应的规则："))
+    out_start_num = int(input("请输入相应的开始位置："))
+    return out_rule,out_start_num
 
 
 def out_circle(in_list, in_num, start_index=1):
@@ -36,14 +43,12 @@ def out_circle(in_list, in_num, start_index=1):
     in_len = len(in_list)
     count = 0
     move = start_index - 1
-    index = 0
 
     while in_len > 0:
         count += 1
         if move == in_len:
             move = 0
         if count == in_num:
-            index += 1
             out_list.append(in_list.pop(move))
             count = 0
             move -= 1
@@ -70,6 +75,10 @@ def get_success_person(in_list):
 
 people_list = open_file()
 show_list(people_list)
-out_people = out_circle(people_list, 4, 1)
+rule, start_num = get_input()
+while start_num > PersonInfo.count:
+    print("开始编号输入错误，请输入小于%d的开始下标：" % PersonInfo.count)
+    rule, start_num = get_input()
+out_people = out_circle(people_list, rule, start_num)
 show_list(out_people)
 get_success_person(out_people)
