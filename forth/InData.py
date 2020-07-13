f9dcf0ca03b69337932a173fed7c2a1c1284dee2
liumengling <ling2577879400@163.com>
2020-07-13 21:16:41 +0800

    第四次作业文件

---
+++ forth/InData.py
@@ -0,0 +1,20 @@
+def get_input():
+    out_rule = int(input("请输入相应的规则："))
+    out_start_num = int(input("请输入相应的开始位置："))
+    return out_rule,out_start_num
+
+
+def show_list(in_list):
+    print("淘汰列表：")
+    print("姓名\t性别\t年龄\t电话号码")
+    for x in in_list:
+        if in_list.index(x) == len(in_list)-1:
+            break
+        print(x)
+
+
+def get_success_person(in_list):
+    person = in_list[len(in_list)-1]
+    print("最后胜利的人是：%s" % person.get_name())
+    print("具体信息为：")
+    print(person)
