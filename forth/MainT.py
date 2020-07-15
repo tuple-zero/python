80bcc09892a2aaf4bc0e71dd1012bd2bfe210661
liumengling <ling2577879400@163.com>
2020-07-15 10:46:03 +0800

    第四次作业文件约瑟夫类

--- forth/MainT.py
+++ forth/MainT.py
@@ -4,6 +4,12 @@ import forth.Fileopen as fp
 import forth.InData as ind
 
 
+def test_iter(people_list):
+    test_joseph = joseph.Josephus()
+    test_joseph.add_joseph(people_list)
+    for x in test_joseph:
+        print(x)
+
 people_list = fp.open_file()
 ind.show_list(people_list)
 rule, start_num = ind.get_input()
@@ -14,6 +20,9 @@ while start_num > person.PersonInfo.count or start_num < 1:
 
 joseph_test = joseph.Josephus()
 joseph_test.add_joseph(people_list)
+#测试迭代器
+test_iter(people_list)
+
 assert (len(joseph_test.people) == len(people_list))
 joseph_test.step = rule
 joseph_test.start = start_num
