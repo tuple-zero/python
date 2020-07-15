9fd253a381ec43283bb6fd5e8a543901efebd1b8
liumengling <ling2577879400@163.com>
2020-07-15 14:49:50 +0800

    第四次作业文件约瑟夫类

--- forth/Person.py
+++ forth/Person.py
@@ -9,7 +9,10 @@ class PersonInfo(object):
         PersonInfo.count += 1
 
     def __str__(self):
-        return "%s\t  %s\t  %s\t %s" % (self._name, self._gender, self._age, self._number)
+        sex = '男'
+        if self._gender == 0:
+            sex = '女'
+        return "%s\t  %s\t  %d\t %s" % (self._name, sex, self._age, self._number)
 
     def get_name(self):
         return self._name
