8613f6f519b8f6122a193805481aee669545eb58
liumengling <ling2577879400@163.com>
2020-07-15 14:49:18 +0800

    第四次作业文件约瑟夫类

--- forth/Fileopen.py
+++ forth/Fileopen.py
@@ -6,6 +6,9 @@ def open_file(file_path):
     with open(file_path, "r", encoding="utf-8") as out_file:
         for line in out_file:
             person_list = line.split()
-            a_person = person.PersonInfo(person_list[0], person_list[1], person_list[2], person_list[3])
+            sex = 0
+            if person_list[1] == '男':
+                sex = 1
+            a_person = person.PersonInfo(person_list[0], sex, int(person_list[2]), person_list[3])
             out_list.append(a_person)
     return out_list
